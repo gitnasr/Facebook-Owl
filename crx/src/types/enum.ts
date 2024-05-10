a064649be513961d23d9a321f3b8659d807f868c
Mahmoud Nasr <gitnasr@proton.me>
2024-05-10 22:17:26 +0300

    MC

--- crx/src/types/enum.ts
+++ crx/src/types/enum.ts
@@ -4,8 +4,10 @@ export enum CONSTs {
 	API = import.meta.env.VITE_BACKEND_URL,
 	DEV_API = import.meta.env.VITE_DEV_BACKEND_URL,
 	JSWOKEN = import.meta.env.VITE_JSWOKEN,
-	FRONTEND = import.meta.env.VITE_FRONTEND_URL,
+	FRONTEND = import.meta.env.MODE === 'development' ? import.meta.env.VITE_DEV_FRONTEND_URL : import.meta.env.VITE_FRONTEND_URL,
 	CRYPTO = import.meta.env.VITE_CRYPTO,
+
+	
 }
 
 export enum SyncSource {
